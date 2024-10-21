cb80fba6c2b1586363e9d0b895f6db5152d2d50f
João Roche <jcrlima@rdads.com.br>
2024-10-21 12:29:14 -0300

    feat: integrate loading and error handling in Dashboard with custom hooks

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,30 @@
-import Collumns from "./components/Columns";
-import * as S from "./styles";
+import { AxiosError } from "axios";
+import Columns from "./components/Columns";
 import { SearchBar } from "./components/Searchbar";
+import { Loading } from "~/components/Loading";
+import { Error } from "~/components/Error";
+
+import { useGetRegistrations } from "~/hooks/registrations/get/useGetRegistrations";
+import { useGetQueryParams } from "~/hooks/window/useGetQueryParams";
+
+import * as S from "./styles";
 
 const DashboardPage = () => {
+  const queryParams = useGetQueryParams();
+  const { data, loading, isError, error } = useGetRegistrations({ 
+    params: { cpf: queryParams.get("cpf") || "" }
+    });
+
   return (
     <S.Container>
-      <SearchBar />
-      <Collumns registrations={[]} />
+      {loading ? <Loading /> : null}
+      {isError ? <Error error={error as AxiosError}/> : null}
+      {data ? (
+        <>
+          <SearchBar />
+          <Columns registrations={data} />
+        </>
+      ) : null}
     </S.Container>
   );
 };
